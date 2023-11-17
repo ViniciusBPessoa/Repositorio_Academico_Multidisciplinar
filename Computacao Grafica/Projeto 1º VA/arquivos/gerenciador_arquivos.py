670b34c4466ec03034672964fe759217e663addc
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2023-11-17 00:31:39 -0300

    Subindo carregador de malha do projeto de computação gráfica
    Subindo carregador de malha do projeto de computação gráfica
    
    ainda restante os comentarios

--- Computacao Grafica/Projeto 1º VA/arquivos/gerenciador_arquivos.py
+++ Computacao Grafica/Projeto 1º VA/arquivos/gerenciador_arquivos.py
@@ -1,6 +1,46 @@
-def carregar_arquivo_modelo(nome_modelo="default"):
-    with open(f"/modelos/{nome_modelo}.txt", "r") as modelo:
-        linhas = modelo.readline
-    return linhas
+import os
 
-print(carregar_arquivo_modelo("casa"))
+class GerenciadorModelo:
+    def __init__(self):
+        self.diretorio_modelos = os.path.dirname(os.path.abspath(__file__)) + "/modelos/"
+        self.malha_atual = None
+
+    def carregar_malha(self, malha):
+        arquivo_malha = self.diretorio_modelos + f"{malha}.byu"
+
+        try:
+            with open(arquivo_malha, 'r') as malha_carregada:
+                linhas = malha_carregada.readlines()
+                num_vertices, num_triangulos = map(int, linhas[0].split())
+                vertices = []
+                faces = []
+
+                # Lendo os vértices
+                for linha in linhas[1:num_vertices + 1]:
+                    coordenadas = list(map(float, linha.split()))
+                    vertices.append(coordenadas)
+
+                # Lendo as faces (triângulos)
+                for linha in linhas[num_vertices + 1 : num_vertices + num_triangulos + 1]:
+                    indices = list(map(int, linha.split()))
+                    faces.append(indices)
+
+                self.malha_atual = {'vertices': vertices, 'faces': faces}
+        
+        except FileNotFoundError:
+            print(f"O arquivo '{arquivo_malha}' não foi encontrado.")
+
+    def exibir_malha(self):
+        if self.malha_atual != None:
+            print("Vértices da Malha Carregada:")
+            for indice, vertice in enumerate(self.malha_atual['vertices'], start=1):
+                print(f"Vértice {indice}: {vertice}")
+
+            print("\nFaces (Triângulos) da Malha Carregada:")
+            for indice, face in enumerate(self.malha_atual['faces'], start=1):
+                print(f"Face {indice}: {face}")
+
+# Exemplo de uso
+ca = GerenciadorModelo()
+ca.carregar_malha("piramide")
+ca.exibir_malha()
