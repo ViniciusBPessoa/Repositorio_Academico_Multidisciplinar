e07f294a509072dd823bfa34b228f984f5b6daf4
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2023-11-17 01:11:59 -0300

    comentários adicionados

--- Computacao Grafica/Projeto 1º VA/arquivos/gerenciador_arquivos.py
+++ Computacao Grafica/Projeto 1º VA/arquivos/gerenciador_arquivos.py
@@ -1,36 +1,39 @@
 import os
 
-class GerenciadorModelo:
+# esse codigo temo objetivo de carregar minha malha na memoria formando as duas tabelas em formato de dicionario
+
+class GerenciadorModelo: # responsavel por gerenciar o carregamento do modelo
     def __init__(self):
-        self.diretorio_modelos = os.path.dirname(os.path.abspath(__file__)) + "/modelos/"
-        self.malha_atual = None
+        self.diretorio_modelos = os.path.dirname(os.path.abspath(__file__)) + "/modelos/" # Para executar sempre (Carrega a devida localização)
+        self.malha_atual = None # mantem a malha atravez de um dicionario
 
-    def carregar_malha(self, malha):
-        arquivo_malha = self.diretorio_modelos + f"{malha}.byu"
+    def carregar_malha(self, malha): # carrega a malha
+        arquivo_malha = self.diretorio_modelos + f"{malha}.byu" # localiza o arquivo
 
-        try:
-            with open(arquivo_malha, 'r') as malha_carregada:
-                linhas = malha_carregada.readlines()
-                num_vertices, num_triangulos = map(int, linhas[0].split())
+        try: # try (para n dar merda)
+            with open(arquivo_malha, 'r') as malha_carregada: # abre o arquiivo
+                linhas = malha_carregada.readlines() # carrega as linhas do arquivo
+                num_vertices, num_triangulos = map(int, linhas[0].split()) 
                 vertices = []
                 faces = []
 
-                # Lendo os vértices
+                # carregando os vertices
                 for linha in linhas[1:num_vertices + 1]:
                     coordenadas = list(map(float, linha.split()))
                     vertices.append(coordenadas)
 
-                # Lendo as faces (triângulos)
+                # carregando as faces (triângulos)
                 for linha in linhas[num_vertices + 1 : num_vertices + num_triangulos + 1]:
                     indices = list(map(int, linha.split()))
                     faces.append(indices)
 
-                self.malha_atual = {'vertices': vertices, 'faces': faces}
-        
-        except FileNotFoundError:
-            print(f"O arquivo '{arquivo_malha}' não foi encontrado.")
+                self.malha_atual = {'vertices': vertices, 'faces': faces} # adicionando o dicionario a variavel correta
+                return self.malha_atual
 
-    def exibir_malha(self):
+        except FileNotFoundError:
+            return -1 # caso o retorno seja -1 o modelo não foi carregado com sucesso
+
+    def exibir_malha(self): # so printa bonitinho
         if self.malha_atual != None:
             print("Vértices da Malha Carregada:")
             for indice, vertice in enumerate(self.malha_atual['vertices'], start=1):
