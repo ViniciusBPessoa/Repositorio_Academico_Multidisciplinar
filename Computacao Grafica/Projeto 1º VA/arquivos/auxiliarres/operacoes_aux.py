679529897ec839ea77c0f37afa999e1e9ff319ea
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2023-11-22 00:26:22 -0300

    Update operacoes_aux.py

--- Computacao Grafica/Projeto 1º VA/arquivos/auxiliarres/operacoes_aux.py
+++ Computacao Grafica/Projeto 1º VA/arquivos/auxiliarres/operacoes_aux.py
@@ -1,11 +1,23 @@
 import numpy as np
 
-# Seus dados
-V = np.array([-1.0, -1.0, -1.0])
-N = np.array([0.0, 0.0, 1.0])
+def projecao_ortogonal(u, v):
+    u = np.array(u)
+    v = np.array(v)
+    
+    # Calcula o produto escalar entre u e v
+    produto_escalar = np.dot(u, v)
+    
+    # Calcula o produto escalar entre u e u
+    produto_escalar_uu = np.dot(u, u)
+    
+    # Calcula a projeção ortogonal de v sobre u
+    proj_u_v = (produto_escalar / produto_escalar_uu) * u
+    
+    return proj_u_v
 
-# Processo de Gram-Schmidt
-V -= np.dot(N, V) * N  # Remova a componente de V na direção de N
-V /= np.linalg.norm(V)  # Normalize V
+# Exemplo de utilização da função
+vetor_u = [1, 2, 3]
+vetor_v = [2.28571429, 4.57142857, 6.85714286]
 
-print("V corrigido:", V)
+projetado = projecao_ortogonal(vetor_u, vetor_v)
+print("Projeção ortogonal de v sobre u:", projetado)
