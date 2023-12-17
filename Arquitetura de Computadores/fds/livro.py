d7049c4b810f4a04a54b654ed13900c8c395e4c8
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2023-12-17 20:09:13 -0300

    rest

--- Arquitetura de Computadores/fds/livro.py
+++ Arquitetura de Computadores/fds/livro.py
@@ -7,4 +7,4 @@ class Livro():
         self.Historico_venda = [[Nome_vendedor, data_hora, valor, qtd], [Nome_vendedor, data_hora, valor, qtd],[Nome_vendedor, data_hora, valor, qtd]]
 
     def fds():
-        pass
+        return 1
