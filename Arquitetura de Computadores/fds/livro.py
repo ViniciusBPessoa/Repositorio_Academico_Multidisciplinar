3219340fdbb54e619cac521b07bce9caaaf54b1b
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2023-12-17 20:07:33 -0300

    teste de aprendisado
    asDASF

---
+++ Arquitetura de Computadores/fds/livro.py
@@ -0,0 +1,10 @@
+class Livro():
+    def __init__(self, nome, autor, qtd) -> None:
+        self.nome = nome
+        self.autor = autor
+        self.quantidade = qtd
+        self.Historico = [[Nome_vendedor, data_hora, valor, qtd], [Nome_vendedor, data_hora, valor, qtd],[Nome_vendedor, data_hora, valor, qtd]]
+        self.Historico_venda = [[Nome_vendedor, data_hora, valor, qtd], [Nome_vendedor, data_hora, valor, qtd],[Nome_vendedor, data_hora, valor, qtd]]
+
+    def fds():
+        pass
