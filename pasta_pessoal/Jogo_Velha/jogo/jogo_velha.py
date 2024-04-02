e3efe22a4b18a105863fd4a0a9315d4ac5285fab
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2024-04-02 17:58:56 -0300

    sistema verificador de vitoria

--- pasta_pessoal/Jogo_Velha/jogo/jogo_velha.py
+++ pasta_pessoal/Jogo_Velha/jogo/jogo_velha.py
@@ -1,8 +1,34 @@
 class jogo_velha:
-    def __init__(self) -> None:
-        self.tabuleiro = [0 for i in range(9)]
-        self.player_1 = 'X'
-        self.player_2 = 'O'
+    def __init__(self, players = ['X', 'O']) -> None:
+        self.tabuleiro = ['N' for i in range(9)]
+        self.players = players
 
-    def victory_checker():
-        pass
+
+    # verify if one of the players wins the game
+    def victory_checker(self):
+        # Listas de combinações de vitória
+        victory_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Linhas
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Colunas
+            [0, 4, 8], [2, 4, 6]              # Diagonais
+        ]
+        winer = 'N'
+        
+        for combination in victory_combinations:
+            for player in self.players:
+                verifyer = True
+                for analitc_position in combination:
+                    if self.tabuleiro[analitc_position] != player:
+                        verifyer = False
+
+                if verifyer:
+                    winer = player
+    
+        return winer
+    
+    def stap(self, location, player):              
+        if self.tabuleiro[location-1] == "N":
+            self.tabuleiro[location-1] = self.players[player]
+
+test_tab = jogo_velha()
+print(test_tab.tabuleiro)
