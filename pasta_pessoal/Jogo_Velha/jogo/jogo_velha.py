d4bbe17ff4fd9b7c645ab1b26bb5a29794160511
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2024-04-02 00:31:54 -0300

    o inicio de um sonho

---
+++ pasta_pessoal/Jogo_Velha/jogo/jogo_velha.py
@@ -0,0 +1,8 @@
+class jogo_velha:
+    def __init__(self) -> None:
+        self.tabuleiro = [0 for i in range(9)]
+        self.player_1 = 'X'
+        self.player_2 = 'O'
+
+    def victory_checker():
+        pass
