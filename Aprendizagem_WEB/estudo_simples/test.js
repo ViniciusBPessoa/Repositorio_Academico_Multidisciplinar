7ef148c08d788599141390be12a11ebad589e65e
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2024-10-28 19:43:21 -0300

    n lembro

---
+++ Aprendizagem_WEB/estudo_simples/test.js
@@ -0,0 +1,8 @@
+function clicando(){
+    var but_2 = document.getElementById('tue');
+    var recebendo = "Link FODA PARA CARALHO";
+    but_2.innerHTML = recebendo
+}
+function tuezin(){
+    window.location.href = 'https://www.youtube.com/watch?v=TKqa-ZXGKYA';
+}
