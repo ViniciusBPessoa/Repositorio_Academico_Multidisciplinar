8b34fb7343e79a979b56a693708cab17a1e60e88
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2024-11-06 19:08:50 -0300

    Sistema do jogo da velha concluido

--- pasta_pessoal/Jogo_Velha/jogo/jogo_velha.py
+++ pasta_pessoal/Jogo_Velha/jogo/jogo_velha.py
@@ -64,6 +64,8 @@ class jogo_velha:
         self.tabuleiro = ['N' for i in range(9)]
         self.players = players
 
+    def get_tat
+
 test_tab = jogo_velha()
 test_tab.tabuleiro = ['X', 'X', 'N', 'N', 'X', 'N', 'N', 'N', 'N']
 
