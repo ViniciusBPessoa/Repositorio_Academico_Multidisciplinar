9fc4aeab40448366f700f09153436f3f7d65b73d
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2025-01-23 16:26:34 -0300

    Modificações e aprendizado

--- Aprendizagem_WEB/Pessoal/src/App.js
+++ Aprendizagem_WEB/Pessoal/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>opa</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "Vinicius",
+      novoNome: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ novoNome: event.target.value });
+  }
+
+  handleClick = () => {
+    this.setState({ nome: this.state.novoNome });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Olá, {this.state.nome}!</p>
+        <input type="text" value={this.state.novoNome} onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Alterar Nome</button>
+      </div>
+    );
+  }
 }
 
 export default App;
