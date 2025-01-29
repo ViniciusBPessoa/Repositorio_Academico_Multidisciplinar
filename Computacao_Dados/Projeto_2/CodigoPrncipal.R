c3ee7aa6a3bcd302b254ed8659d5877305d3e66a
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2025-01-29 17:57:22 -0300

    subindo essa parada ai

---
+++ Computacao_Dados/Projeto_2/CodigoPrncipal.R
@@ -0,0 +1,6 @@
+getwd()
+setwd("C:/Faculdade/Repositorios/Repositorio_Academico_Multidisciplinar/Computacao_Dados/Projeto_2")
+
+dados <- read.csv("Crime_Data_from_2020_to_Present.csv")
+
+print(head(dados))
