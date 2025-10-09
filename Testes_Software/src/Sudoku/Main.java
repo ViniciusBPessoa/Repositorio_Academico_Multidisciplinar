ee67d9109dcaf4b8f8b3ac20ddba5045f304f627
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2025-10-09 02:37:55 -0300

    Atualizaçoes

---
+++ Testes_Software/src/Sudoku/Main.java
@@ -0,0 +1,12 @@
+package Sudoku;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Casa");
+        
+        // Versão do Java
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
+        System.out.println("Java Home: " + System.getProperty("java.home"));
+    }
+}
---
+++ Testes_Software/src/Sudoku/Sudoku.java
@@ -0,0 +1,7 @@
+package Sudoku;
+
+public class Sudoku {
+	
+	
+
+}
